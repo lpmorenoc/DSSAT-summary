55c568d11da32a27a16647d1a7b667659093fc56
Daniel Amariles <daniel.amariles88@gmail.com>
2017-06-13 16:14:05 -0500

    fixing split.bat

--- split.bat
+++ split.bat
@@ -1,7 +1,7 @@
 @echo off
 setLocal EnableDelayedExpansion
 
-set limit=50001
+set limit=100001
 set file=eval.json
 set lineCounter=1
 set filenameCounter=1
@@ -16,12 +16,13 @@ for %%a in (%file%) do (
 
 for /f "tokens=*" %%a in (%file%) do (
     set splitFile=!name!-part!filenameCounter!!extension!
-    if !lineCounter! gtr !limit! ( IF !lineCounter! %2 EQU 0 (
+    if !lineCounter! gtr !limit! (
         set /a filenameCounter=!filenameCounter! + 1
         set lineCounter=1
         echo Created !splitFile!.
         echo curl -k -i --raw -o !splitFile!.log -X POST "http://localhost:9200/evaluation/_bulk?pretty" -H "Content-Type: application/json" -H "User-Agent: Fiddler" -H "Host: localhost:9200" --data-binary @!splitFile!>>!uploaderFile!
-    ))
-    echo %%a>> !splitFile!
-    set /a lineCounter=!lineCounter! + 1
+    )
+     echo %%a>> !splitFile!
+     set /a lineCounter=!lineCounter! + 1
+
 )
