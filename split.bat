1f51d2a307f5f1e65fe21d211ba97c58b0a0eff9
Daniel Amariles <daniel.amariles88@gmail.com>
2017-06-14 08:19:02 -0500

    fixing split to start subfile in index statement

--- split.bat
+++ split.bat
@@ -1,28 +1,34 @@
 @echo off
 setLocal EnableDelayedExpansion
 
-set limit=100001
+del uploader.bat
+del eval-part*
+set limit=100000
 set file=eval.json
-set lineCounter=1
+set lineCounter=0
 set filenameCounter=1
 set uploaderFile=uploader.bat
-
+set quote=brute
 set name=
 set extension=
 for %%a in (%file%) do (
-    set "name=%%~na"
-    set "extension=%%~xa"
+	set "name=%%~na"
+	set "extension=%%~xa"
 )
 
 for /f "tokens=*" %%a in (%file%) do (
-    set splitFile=!name!-part!filenameCounter!!extension!
-    if !lineCounter! gtr !limit! (
-        set /a filenameCounter=!filenameCounter! + 1
-        set lineCounter=1
-        echo Created !splitFile!.
-        echo curl -k -i --raw -o !splitFile!.log -X POST "http://localhost:9200/evaluation/_bulk?pretty" -H "Content-Type: application/json" -H "User-Agent: Fiddler" -H "Host: localhost:9200" --data-binary @!splitFile!>>!uploaderFile!
-    )
-     echo %%a>> !splitFile!
-     set /a lineCounter=!lineCounter! + 1
+	set splitFile=!name!-part!filenameCounter!!extension!
+	set line=%%a
+	if !lineCounter! gtr !limit! ( 
+		if "!line:~2,5!" equ "!quote!" (
+			set /a filenameCounter=!filenameCounter! + 1
+			set lineCounter=0
+			echo Created !splitFile!.
+			echo curl -k -i --raw -o !splitFile!.log -X POST "http://localhost:9200/evaluation/_bulk?pretty" -H "Content-Type: application/json" -H "User-Agent: Fiddler" -H "Host: localhost:9200" --data-binary @!splitFile!>>!uploaderFile!
+		)
+	)
+	echo !line!>> !splitFile!
+	set /a lineCounter=!lineCounter! + 1
 
 )
+
